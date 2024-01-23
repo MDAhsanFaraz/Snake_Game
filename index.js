8b4b6178bcf0e81f72f8934310431f42c496c6ee
Faraz <faraz.com>
2024-01-23 18:26:18 +0530

    minor mistake fixed

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const scoreBoard = document.createElement("div");
     scoreBoard.id = "score-board";
     document.body.insertBefore(scoreBoard, gameArena);
+    const startButton = document.createElement("button");
+    startButton.textContent = "Start Game";
+    startButton.classList.add("start-button");
+    document.body.appendChild(startButton);
   }
-  const startButton = document.createElement("button");
-  startButton.textContent = "Start Game";
-  startButton.classList.add("start-button");
-  document.body.appendChild(startButton);
+
+  startGame();
 });
