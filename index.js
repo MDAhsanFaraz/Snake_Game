1ae269badc8587fbf01212962dc6df2655f6ec1d
Faraz <faraz.com>
2024-01-25 23:28:11 +0530

    Completed game logic with game speed

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
   let dx = cellSize; // displacement on x axis
   let dy = 0; // displacement on y axis
 
+  let gameSpeed = 200;
+  let intervalId;
+
   function drawScoreBoard() {
     const scoreBoard = document.getElementById("score-board");
     scoreBoard.textContent = `Score: ${score}`;
@@ -44,12 +47,12 @@ document.addEventListener("DOMContentLoaded", () => {
     // again re initiate the random call if the random coordinates
     // don't match with any of the snake cell stop
     do {
-      newX = Math.floor(
-        Math.random() * ((arenaSize - cellSize) / cellSize) * cellSize
-      );
-      newY = Math.floor(
-        Math.random() * ((arenaSize - cellSize) / cellSize) * cellSize
-      );
+      newX =
+        Math.floor(Math.random() * ((arenaSize - cellSize) / cellSize)) *
+        cellSize;
+      newY =
+        Math.floor(Math.random() * ((arenaSize - cellSize) / cellSize)) *
+        cellSize;
     } while (
       snake.some((snakeCell) => snakeCell.x === newX && snakeCell.y === newY)
     );
@@ -63,6 +66,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (newHead.x === food.x && newHead.y === food.y) {
       // collision
       score += 5;
+      if (gameSpeed > 30) {
+        clearInterval(intervalId);
+        gameSpeed -= 10;
+        gameLoop();
+      }
       // dont pop the tail
       moveFood();
       // move the food
@@ -88,7 +96,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function gameLoop() {
-    setInterval(() => {
+    intervalId = setInterval(() => {
       if (!gameStarted) return;
       // check for gameover
       if (isGameOver()) {
@@ -100,12 +108,51 @@ document.addEventListener("DOMContentLoaded", () => {
       updateSnake();
       drawScoreBoard();
       drawFoodAndSnake();
-    }, 100);
+    }, gameSpeed);
+  }
+
+  function changeDirection(e) {
+    console.log(e, e.keyCode);
+
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+
+    const keyPressed = e.keyCode;
+
+    const isGoingUp = dy == -cellSize;
+    const isGoingDown = dy == cellSize;
+    const isGoingLeft = dx == -cellSize;
+    const isGoingRight = dx == cellSize;
+
+    if (keyPressed == LEFT_KEY && !isGoingRight) {
+      dy = 0;
+      dx = -cellSize;
+    }
+
+    if (keyPressed == RIGHT_KEY && !isGoingLeft) {
+      dy = 0;
+      dx = cellSize;
+    }
+
+    if (keyPressed == UP_KEY && !isGoingDown) {
+      dy = -cellSize;
+      dx = 0;
+    }
+
+    if (keyPressed == DOWN_KEY && !isGoingUp) {
+      dy = cellSize;
+      dx = 0;
+    }
   }
 
   function runGame() {
-    gameStarted = true;
-    gameLoop();
+    if (!gameStarted) {
+      gameStarted = true;
+      gameLoop();
+      document.addEventListener("keydown", changeDirection);
+    }
   }
 
   function initiateGmae() {
