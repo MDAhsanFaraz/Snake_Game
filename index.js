065eeec16925e8654219efd146bb36919bde2d07
Faraz <faraz.com>
2024-01-25 16:49:41 +0530

    Added food item in the game arena

--- index.js
+++ index.js
@@ -13,15 +13,52 @@ document.addEventListener("DOMContentLoaded", () => {
   let dx = cellSize; // displacement on x axis
   let dy = 0; // displacement on y axis
 
-  function startGame() {
+  function drawScoreBoard() {
+    const scoreBoard = document.getElementById("score-board");
+    scoreBoard.textContent = `Score: ${score}`;
+  }
+
+  function drawDiv(x, y, className) {
+    const div = document.createElement("div");
+    div.classList.add(className);
+    div.style.top = `${y}px`;
+    div.style.left = `${x}px`;
+    return div;
+  }
+  function drawFoodAndSnake() {
+    gameArena.innerHTML = ""; //if previously something is drawn remove it
+    // wipe out everything and redraw with new coordinates when snake moves
+    const foodElement = drawDiv(food.x, food.y, "food");
+    gameArena.appendChild(foodElement);
+  }
+
+  function gameLoop() {
+    setInterval(() => {
+      drawScoreBoard();
+      drawFoodAndSnake();
+    }, 1000);
+  }
+
+  function runGame() {
+    gameStarted = true;
+    gameLoop();
+  }
+
+  function initiateGmae() {
     const scoreBoard = document.createElement("div");
     scoreBoard.id = "score-board";
     document.body.insertBefore(scoreBoard, gameArena);
+
     const startButton = document.createElement("button");
     startButton.textContent = "Start Game";
     startButton.classList.add("start-button");
     document.body.appendChild(startButton);
+
+    startButton.addEventListener("click", () => {
+      startButton.style.display = "none";
+      runGame();
+    });
   }
 
-  startGame();
+  initiateGmae(); // this is the first function to be executed so that we prepare the ui
 });
